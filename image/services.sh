f57f62056755885e42f7bf62d2007a03f0491102
Emanuele Peruffo <info@emanueleperuffo.com>
2015-05-19 18:38:33 +0200

    fixing service

--- image/services.sh
+++ image/services.sh
@@ -3,8 +3,12 @@ set -e
 source /build/buildconfig
 set -x
 
-## Installing Postgres
+## Installing postgres
 $minimal_apt_get_install postgresql-${PG_MAJOR} postgresql-contrib-${PG_MAJOR} postgis-${POSTGIS_VERSION}
 
+## Setting up postgres service
+mkdir /etc/service/postgres
+cp /build/runit/postgres /etc/service/postgres/run
+
 ## Remote syslog
-cp /build/config/syslog-ng/conf.d/20-postgres-remote.conf /etc/config/syslog-ng/
+cp /build/config/syslog-ng/conf.d/20-postgres-remote.conf /etc/syslog-ng/conf.d/
