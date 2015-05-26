43634fe9231299da2f22e201d4dfdfea413875c4
Emanuele Peruffo <info@emanueleperuffo.com>
2015-05-26 11:16:58 +0200

    removed syslog conf, removed versions from Dockerfile, using the bootstrapped variable

--- image/services.sh
+++ image/services.sh
@@ -6,11 +6,8 @@ set -x
 ## Installing postgres
 $minimal_apt_get_install postgresql-common
 sed -ri 's/#(create_main_cluster) .*$/\1 = false/' /etc/postgresql-common/createcluster.conf
-$minimal_apt_get_install postgresql-${PG_MAJOR} postgresql-contrib-${PG_MAJOR}
+$minimal_apt_get_install postgresql-${POSTGRES_VERSION} postgresql-contrib-${POSTGRES_VERSION}
 
 ## Setting up postgres service
 mkdir /etc/service/postgres
 cp /build/runit/postgres /etc/service/postgres/run
-
-## Remote syslog
-cp /build/config/syslog-ng/conf.d/20-postgres-remote.conf /etc/syslog-ng/conf.d/
